0fd826f6ba5266765cf60f162b2ca0299b016450
JamesHansIV <jameshanselmaniv@ku.edu>
2022-04-24 12:17:08 -0500

    added sign-in.html

---
+++ sign-in.js
@@ -0,0 +1,16 @@
+//placeholder vars
+let user = [{username: 'user', password: 'password'}];
+
+//SIGN IN
+const signIn = () => {
+    //get value of fields
+
+    //clear fields
+
+    //query database and see if login is correct
+
+
+    
+    
+    //set user token
+}
